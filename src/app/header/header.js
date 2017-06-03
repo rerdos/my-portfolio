ea4b9b328af99c08384f4f3adaba0ea783df5e3d
Richard Erdos <richard.erdos@me.com>
2017-06-03 17:01:14 +0100

    Finish header component

--- src/app/header/header.js
+++ src/app/header/header.js
@@ -1,6 +1,18 @@
 import template from './header.html';
+import headerData from './../../data/header.json';
+
+class HeaderCtrl {
+  $onInit() {
+    const $ctrl = this;
+
+    $ctrl.data = headerData;
+  }
+}
 
 export default angular
   .module('app.header', [])
-  .component('appHeader', { template })
+  .component('appHeader', {
+    template,
+    controller: HeaderCtrl
+  })
   .name;
